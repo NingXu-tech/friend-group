847b689eb8709a8cc4f9ea41bf98f4067b934e74
NingXu-tech <ning.xu.25@ucl.ac.uk>
2025-10-18 14:55:10 +0100

    Update group.py

--- group.py
+++ group.py
@@ -4,9 +4,12 @@ my_group = {'Jill':{
     'name': 'Jill',
     'age' : 26,
     'job' : 'biologist',
-    'friend': ['Zalika', 'A', 'B'],
-    'partner': ['John'],
-    'colleague': []
+    'relations' :{
+        'friend': ['Zalika', 'A', 'B'],
+        'partner': ['John'],
+        'colleague': []
+    }
+    
 }}
 
 # print(my_group['Jill'].get('job'))
@@ -14,11 +17,11 @@ my_group = {'Jill':{
 def getJob(name): # getter
     print(my_group[name].get('job'))
 
-def addPerson(name, job, age):
-    print('added person: ', name, job, age)
+def addPerson(name, job, age， relations):
+    print('added person: ', name, job, age, relations)
     # add the person into the dictionary my_group
 
-addPerson('Zalika', 'artist', 28)
+addPerson('Zalika', 'artist', 28, {'friend' :['jill']})
 
 # def setJob(name, jobName): # setter
 #     my_group[name].job = jobName
